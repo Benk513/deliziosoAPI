79f4324e6dbfa2f6462edc13aa059ca4782fcc5f
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-16 15:31:02 +0100

    feat(MenuModel) :add menu model for storing menus

---
+++ models/menuModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+const validator =require ('validator')
+
+const menuSchema =mongoose.Schema({
+    imageURL: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    name: {
+        type: String,         
+        required: [true, 'A menu must have a name'],
+        unique: true,
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+        minlength: [4, 'A tour name must have more or equal then 4 characters'],
+        validate: [validator.isAlpha , 'a menu name must only contain characters ']
+    },
+    
+    category: {
+        type: String,
+        trim: true,
+        required: [true, 'A menu must have a category'],
+    },
+
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A menu must have a description']
+    },
+
+    ingredients: [String],
+
+    price: {
+        type: Number,
+        required: [true, 'A menu must have a price']
+      },
+
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
+    },
+
+    rating: Number,
+    //reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+    
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0']
+      },
+  
+
+    createdAt: { type: Date, default: Date.now},
+    updatedAt: { type: Date, default: Date.now },
+
+    availability:Boolean
+
+ 
+})
+
+module.exports = mongoose.model('Menu', menuSchema)
+ 
