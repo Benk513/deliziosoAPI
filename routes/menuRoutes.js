e53bf02a82a1587ff0b0cd16a13cd7b83e743cfb
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-08-16 16:12:19 +0100

    feat(menuRoutes):add menu routes features

---
+++ routes/menuRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const menuController = require('./../controllers/menuController')
+
+const router = express.Router()
+
+router
+  .route('/')
+  .get(menuController.getAllMenus)
+    .post(menuController.createMenu);
+  
+router.route('/:id')
+    .get(menuController.getMenu)
+    .patch(menuController.updateMenu)
+    .delete(menuController.deleteMenu)
+
+
+module.exports = router;
