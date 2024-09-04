59c09019cce736721890b35261a51dedb4ae4399
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-04 18:00:51 +0100

    fix(AppError): change AppError to Error for the stacktrace

--- utils/appError.js
+++ utils/appError.js
@@ -5,10 +5,8 @@ class AppError extends Error{
         this.statusCode = statusCode
         this.status =`${statusCode}`.startsWith('4') ? 'fail' : 'error';
 
-        AppError.captureStackTrace(this, this.constructor)
+        Error.captureStackTrace(this, this.constructor)
 
     }
 }
-
-
 module.exports = AppError
