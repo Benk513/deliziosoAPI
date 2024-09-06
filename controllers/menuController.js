4e331cff1d8a1cbc50d5b5b970ba3c68c0447349
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-06 16:42:59 +0100

    feat(bad ID) : if the id is wrong but i in a correct format than send this error 404

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -2,7 +2,7 @@ const Menu = require('./../models/menuModel')
 const APIFeatures = require('./../utils/apiFeatures')  
 const catchAsync = require('./../utils/catchAsync')
 const AppError = require('./../utils/appError')
- 
+
 exports.getAllMenus = catchAsync( async(req, res, next) => {
         //EXECUTE QUERY       
         const features = new APIFeatures(Menu.find(), req.query)
@@ -41,13 +41,13 @@ exports.createMenu = catchAsync(async (req, res,next) => {
 })
 
 exports.updateMenu =  catchAsync(async (req, res, next) => {
-    const id = req.params.id
-    
+    const id = req.params.id    
      
         const menu = await Menu.findByIdAndUpdate(id, req.body, {
             new: true,
             runValidators: true
         })
+        if(!menu) return next(new AppError('No menu found with that ID', 404))
 
         res.status(200).json({
             status: 'success',
@@ -57,7 +57,9 @@ exports.updateMenu =  catchAsync(async (req, res, next) => {
 
 exports.deleteMenu =catchAsync(  async (req, res, next) => {
     const id = req.params.id*1
-        await Menu.findByIdAndDelete(id)
+      const menu= await Menu.findByIdAndDelete(id)
+
+        if(!menu) return next(new AppError('No menu found with that ID', 404))
         res.status(204).json({
             status: 'success',
             data: null
