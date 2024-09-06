abff8c6415f123becd2ed8c17111acfe3161d2ba
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-06 16:46:54 +0100

    feat(handleCastErrorDB) : to catch a bad id like qdcsddxcscdcd

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,13 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = err => {
+    const message= `Invalid ${err.path} : ${err.value}`
+    // 400 -> bad request
+    return new AppError(message, 400)
+}
+
+
+
 const sendErrorDev =(err,res) =>{
     res.status(err.statusCode).json({
         status:err.status,
@@ -36,6 +46,11 @@ module.exports= (err,req,res,next) =>{
     sendErrorDev(err,res)
    }
    else if(process.env.NODE_ENV ==='production'){
-    sendErrorProd(err, res);
+    let error={...err}
+
+    if(error.name ==="CastError") error = handleCastErrorDB(error);
+    if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if(error.name ==='ValidationError') error = handleValidationErrorDB(error)
+    sendErrorProd(error, res);
    }
-}
+} 
