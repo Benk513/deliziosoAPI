eff9f3fb79771ff9732eb4505197a4e207c291ad
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-11 20:14:09 +0100

    fix(passwordCompare) : with bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -47,6 +47,10 @@ userSchema.pre('save' , async function(next){
     next()
 })
 
+//this is an instance methode for the model and is available for all docs
+userSchema.methods.correctPassword = async function(candidatePassword , userPassword){
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 const User = mongoose.model('User',userSchema)
 
 module.exports = User
