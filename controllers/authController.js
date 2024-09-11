7b271a5dc92ef562919f68d9bbbd6f93f6c8976c
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-11 19:36:06 +0100

    feat(signup):add jwt sign method to create token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,21 @@
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 
 exports.signup = catchAsync(async(req,res,next) =>{
+    // specify the exact attribute you want so the user can not enter himself as role:admin
     const newUser = await User.create({
-        name:req.bpdy.name,
+        name:req.body.name,
         email:req.body.email,
         password:req.body.password,
         passwordConfirm : req.body.passwordConfirm
     });
 
+    const token = jwt.sign({id:newUser._id} , process.env.JWT_SECRET ,{expiresIn:process.env.JWT_EXPIRES_IN})
+
     res.status(201).json({
         status:'success',
+        token,
         data:{
             user:newUser
         }
