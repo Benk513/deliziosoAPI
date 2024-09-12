021b5f77e09661a09050f4b61fc953db5c3b6ba6
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-12 01:40:15 +0100

    fix(app): add the userRouter route

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const morgan = require('morgan')
 const AppError= require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const menuRouter = require('./routes/menuRoutes')
+const userRouter = require('./routes/userRoutes')
 
+// I create the express app
 const app = express()
 
 // MIDDLEWARES
@@ -12,8 +14,7 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.use(express.json())
-
+app.use(express.json()) 
 app.use((req, res, next) => {
 
     if(process.env.NODE_ENV ==="production"){
@@ -27,9 +28,10 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send("hey working here")
+    res.send("Welcome to the Delizioso Restaurant API")
 })
 app.use('/api/v1/menu', menuRouter)
+app.use('/api/v1/users', userRouter)
 
 // app.all('*', (req, res, next) => {
 //     res.status(404).json({
@@ -56,7 +58,6 @@ app.all('*', (req, res, next) => {
 // this middleware is a special middleware because it's catches all the errors , with the error parameter ,
 // express can know that it is a special middleware 
 // au cas ou il ya erreur on saute la chaine des next() et on atterit ici en final dou pas de next, sinon on continue sa course
-
 
 app.use(globalErrorHandler)
 module.exports =app
