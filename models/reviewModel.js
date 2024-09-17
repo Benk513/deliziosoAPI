b855a60827f22a054d17ab322334d2dc126ca87e
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-17 21:30:01 +0100

    added a parent referencing  of menu and user to Review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -16,13 +16,24 @@ const reviewSchema = new mongoose.Schema({
         type:Date,
         default:Date.now()
     },
-
-    updatedAt:Date
     
+
+    updatedAt:Date,
+    menu :{
+        type:mongoose.Schema.ObjectId,
+        ref:'Menu',
+        required:[true, 'Review must belong to a menu']
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User',
+        required:[true, 'Review must belong to a user']
+    }
 })
 
 const Review = mongoose.model('Review', reviewSchema)
 
+module.exports =Review;
 
  
 // user_id (Foreign Key to User.id)
