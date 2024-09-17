aa23b009e6ec5ab33c0f5d224b5a76ad43c8605e
Benk513 <79744201+Benk513@users.noreply.github.com>
2024-09-17 21:25:21 +0100

    feat(reviewModel)

---
+++ models/reviewModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+
+const reviewSchema = new mongoose.Schema({
+
+    review :{
+        type:String,
+        trim:true
+    },
+    rating:{
+        type: Number,
+        min:[1 ,'the rating must be at least 1'],
+        max:[5 , 'the rating must be less or equal to 5']
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+
+    updatedAt:Date
+    
+})
+
+
+ 
+// user_id (Foreign Key to User.id)
+// menu_item_id (Foreign Key to Menu.id)
+ 
+ 
+// created_at (Timestamp)
+// updated_at (Timestamp)
